8f078de744e271688f32dac3836d24d64c64c2b0
Phil Mlaker <philmlaker@gmail.com>
2017-02-10 11:21:51 -0600

    latest 11:21A

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+// exports.connection = mysql.createConnection({
+// 	host:'localhost',
+// 	user:'root',
+// 	password:'',
+// 	database:'employeeRegistry'
+// });
+
+exports.connection = function connect(app) {
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employeeRegistry'
+    });
+
+
+    connection.connect(function(err) {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+
+        console.log("connected as id " + connection.threadId);
+
+    });
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mysql = require('mysql');
+var router = express.Router();
+var connectionimport = require('./config/connection.js');
+var methodOverride = require("method-override");
+var app = express();
+var port = 3000;
+
+
+connectionimport.connection(app);
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// MICHAEL - HOW DO I GET CONNECTION LINKED UP TO MY MY CONNECTION.JS FILE?
+
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(data);
+    // res.render("index", { burgers: data });
+
+  });
+});
+
+
+
+app.listen(port);
